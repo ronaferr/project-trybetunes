f99bdd6c63d0dec24b711a3f14821c9306b6a779
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-04-20 16:53:48 -0300

    entrega requisito 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.GetInfo = this.GetInfo.bind(this);
+    this.state = {
+      info: '',
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.GetInfo();
+  }
+
+  GetInfo = async () => {
+    this.setState({ loading: true });
+    const get = await getUser();
+    this.setState({
+      info: get.name,
+      loading: false,
+    });
+  }
+
   render() {
+    const { info, loading } = this.state;
     return (
       <header data-testid="header-component">
-        <span>ola mundo</span>
+        { loading
+          ? <Loading />
+          : (
+            <span data-testid="header-user-name">{ info }</span>
+          )}
       </header>
     );
   }
