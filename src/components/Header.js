b692ad03b3540e44b914780d689d315404df2880
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-04-20 17:10:55 -0300

    entrega requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -32,7 +33,12 @@ class Header extends React.Component {
         { loading
           ? <Loading />
           : (
-            <span data-testid="header-user-name">{ info }</span>
+            <>
+              <span data-testid="header-user-name">{ info }</span>
+              <Link to="/search" data-testid="link-to-search">Search</Link>
+              <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+              <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+            </>
           )}
       </header>
     );
